9dd8613874f912485d118665c3c245585f8a5855
MCristii <cristii.mocanu@gmail.com>
2019-04-22 21:50:53 +0300

    umplere shop cu produse

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     url(r'', views.product_list, name='product_list'),
     url(r'^(?P<categoryid>\d+)/$', views.product_list, name='product_list_by_category'),
     url(r'^(?P<id>\d+)/$', views.product_detail, name='product_detail'),
